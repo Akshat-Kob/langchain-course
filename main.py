4a11b9fc4632bfbb61c343e1ba155c6b426cbee9
Akshat Shah <akki@Akshats-MacBook-Air.local>
2026-05-07 08:58:50 +0530

    LangSmith : hello-world-1 chain

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def main():
 
     # llm = ChatOpenAI(temperature=0, model="gpt-5")
     llm = ChatGroq(model="llama-3.3-70b-versatile")
-    llm = ChatOllama(temperature=0, model="gemma3:270m")
+    # llm = ChatOllama(temperature=0, model="gemma3:270m")
     chain = summary_prompt_template | llm # LCEL : Langchain expression language
     response = chain.invoke(input={"information": information})
 
