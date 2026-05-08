ead90a01e667bbd818c4467503cd5fa3a8170b2c
Akshat Shah <akki@Akshats-MacBook-Air.local>
2026-05-08 08:23:24 +0530

    Structured output with langchain using Pydantic

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing import List
+from pydantic import BaseModel, Field
+
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -10,12 +13,21 @@ from langchain_groq import ChatGroq
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_tavily import TavilySearch
 
+class Source(BaseModel):
+    """ Schema for a source used by the agent """
+    url: str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """ Schema for the agent response with answer and sources"""
+    answer: str = Field(description="The Agent's answer to the user's query")
+    sources: List[Source] = Field(default_factory=list, description="List of sources used by the agent to generate the answer")
+
 # llm = ChatGroq(model="llama-3.1-8b-instant")
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash"
 )
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
     print("Hello from langchain-course!")
